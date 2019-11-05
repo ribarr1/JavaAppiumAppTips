01a56ce1d799d73005dda409afafc5b75e847fda
Ruben Dario Ibarra Virla <ribarrav@everis.com>
2019-11-04 19:56:37 -0500

    commit proyecto

---
+++ src/main/java/com/riv/movileTest/test1.java
@@ -0,0 +1,15 @@
+package com.riv.movileTest;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+
+public class test1 {
+	
+	
+
+}
